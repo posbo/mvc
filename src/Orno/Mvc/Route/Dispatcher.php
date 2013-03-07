72bed727bde8e9c33410c7c8337ccd7070f7a60b
Phil Bennett <phil@happyaccidents.me>
2013-03-07 22:48:47 +0000

    Added environment set and route matching

--- src/Orno/Mvc/Route/Dispatcher.php
+++ src/Orno/Mvc/Route/Dispatcher.php
@@ -1,5 +1,7 @@
 <?php namespace Orno\Mvc\Route;
 
+use Orno\Mvc\Route\RouteCollection;
+
 class Dispatcher
 {
     /**
@@ -23,21 +25,18 @@ class Dispatcher
     protected $arguments;
 
     /**
+     * @var Orno\Mvc\Route\Route
+     */
+    protected $route;
+
+    /**
      * Constructor
      *
-     * @param string $path
-     * @param string $method
+     * @param Orno\Mvc\Route\RouteCollection $collection
      */
-    public function __construct($path = null, $method = null)
+    public function __construct(RouteCollection $collection)
     {
-        if (is_array($path)) {
-            return $this->setEnvironment();
-        }
-
-        $this->path = $path;
-        $this->method = $method;
-
-        $this->setSegments();
+        $this->collection = $collection;
     }
 
     /**
@@ -55,10 +54,38 @@ class Dispatcher
     /**
      * Set the route environment from the $_SERVER array or mock
      *
-     * @param array $server
+     * @param  array                     $server
+     * @return Orno\Mvc\Route\Dispatcher $this
      */
     public function setEnvironment(array $server = [])
     {
+        $route = str_replace($server['SCRIPT_NAME'], null, $server['REQUEST_URI']);
 
+        $this->path   = ($route === '') ? '/' : $route;
+        $this->method = $server['REQUEST_METHOD'];
+
+        $this->setSegments();
+
+        return $this;
+    }
+
+    public function match($method = 'ALL')
+    {
+        foreach ($this->collection->getRoutes()[$method] as $route) {
+            // is there a literal match?
+            if ($route->getRoute() === $this->path) {
+                $this->route = $route;
+                return true;
+            }
+
+            if (! preg_match('#^' . $route->getRoute() . '$#', $this->path)) {
+                continue;
+            }
+
+            $this->route = $route;
+            return true;
+        }
+
+        return false;
     }
 }
