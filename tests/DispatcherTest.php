517350e6c2bf7fb641940dce0d27c3331649e685
Phil Bennett <phil@happyaccidents.me>
2013-03-09 16:18:12 +0000

    Added methods to test suite

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -6,6 +6,18 @@ use Orno\Mvc\Route\Dispatcher;
 
 class DispatcherTest extends PHPUnit_Framework_Testcase
 {
+    public function testMatchDoesNotExist()
+    {
+        $route = new RouteCollection;
+
+        $route->get('/test', 'TestController@testAction');
+
+        $dispatch = new Dispatcher($route);
+        $dispatch->setEnvironment(['SCRIPT_NAME' => '/index.php', 'REQUEST_URI' => '/index.php', 'REQUEST_METHOD' => 'GET']);
+
+        $this->assertFalse($dispatch->match('GET'));
+    }
+
     public function testMatchOnLiteral()
     {
         $route = new RouteCollection;
@@ -88,5 +100,60 @@ class DispatcherTest extends PHPUnit_Framework_Testcase
         $dispatch->setEnvironment(['SCRIPT_NAME' => '/index.php', 'REQUEST_URI' => '/test', 'REQUEST_METHOD' => 'GET']);
 
         $this->assertTrue($dispatch->match('GET'));
+    }
+
+    public function testDispatchesClosure()
+    {
+        $route = new RouteCollection;
+
+        $route->add('/', function () {
+            return 'Hello World';
+        });
+
+        $dispatch = new Dispatcher($route);
+        $dispatch->setEnvironment(['SCRIPT_NAME' => '/index.php', 'REQUEST_URI' => '/index.php', 'REQUEST_METHOD' => 'GET']);
+
+        $this->assertSame($dispatch->run(), 'Hello World');
+    }
+
+    public function testDispatchesControllerAction()
+    {
+        $route = new RouteCollection;
+
+        $route->add('/', 'Assets\OrnoTest\Controller@index');
+
+        $dispatch = new Dispatcher($route);
+        $dispatch->setEnvironment(['SCRIPT_NAME' => '/index.php', 'REQUEST_URI' => '/index.php', 'REQUEST_METHOD' => 'GET']);
+
+        $this->assertSame($dispatch->run(), 'Hello World');
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testLackOfEnvironmentException()
+    {
+        $route = new RouteCollection;
+
+        $route->add('/', function () {
+            return 'Hello World';
+        });
+
+        $dispatch = new Dispatcher($route);
+
+        $dispatch->run();
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testRouteNotFoundException()
+    {
+        $route = new RouteCollection;
+
+        $dispatch = new Dispatcher($route);
+        $dispatch->setEnvironment(['SCRIPT_NAME' => '/index.php', 'REQUEST_URI' => '/index.php', 'REQUEST_METHOD' => 'GET']);
+
+        $dispatch->run();
     }
 }
