4c46695df8c1f5342055836794f494f52c3dbcaa
Phil Bennett <phil@happyaccidents.me>
2013-03-10 15:57:28 +0000

    Strip query string from route path

--- src/Orno/Mvc/Route/Dispatcher.php
+++ src/Orno/Mvc/Route/Dispatcher.php
@@ -61,6 +61,9 @@ class Dispatcher
     {
         if (! empty($server)) {
             $route = str_replace($server['SCRIPT_NAME'], null, $server['REQUEST_URI']);
+            if (isset($server['QUERY_STRING']) && $server['QUERY_STRING'] !== '') {
+                $route = str_replace('?' . $server['QUERY_STRING'], null, $route);
+            }
             $route = rtrim($route, '/');
             $this->path   = ($route === '') ? '/' : $route;
             $this->method = $server['REQUEST_METHOD'];
